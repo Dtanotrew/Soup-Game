4a6a2c873f56a45fd366fd81ee8ed1b62e5ea160
Aivech <16143392+Aivech@users.noreply.github.com>
2023-12-29 00:24:25 -0500

    removed some test code that shouldn't be there

--- main/common/icon/TintableIcon.gd
+++ main/common/icon/TintableIcon.gd
@@ -1,15 +1,4 @@
 extends TextureRect
 
-var hue
-
 func set_tint(color_rgba):
 	$tint_layer.material.set_shader_parameter("tint_color", color_rgba)
-	
-func _ready():
-	hue = 0
-	set_tint(Color(Color.RED, 1.0))
-
-func _process(delta):
-	hue = hue + delta/5
-	set_tint(Color.from_hsv(hue,1,1))
-	
