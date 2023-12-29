b4952e5d102ca0a08e021b38af804fac9fac51dc
Aivech <16143392+Aivech@users.noreply.github.com>
2023-12-29 13:57:26 -0500

    WORKING DYNAMIC INGREDIENT RENDERS BAYBEEEEE

--- main/brewing-scene/brew_soup.gd
+++ main/brewing-scene/brew_soup.gd
@@ -2,7 +2,14 @@ extends Node2D
 
 signal brew_complete
 
+const tintable_icon = preload("res://main/common/icon/TintableIcon.tscn")
+const potion_main = preload("res://resources/textures/ingredients/Bottle.png")
+const potion_tint = preload("res://resources/textures/ingredients/bottle-tint.png")
+
 func start_brew():
+	var potion = tintable_icon.instantiate()
+	potion.set_textures(potion_main, potion_tint)
+	$BrewPanel/ItemScroller/Items.add_child(potion)
 	#$BrewBG.show()
 	pass
 
@@ -13,5 +20,5 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+#func _process(delta):
+#	pass
--- main/common/icon/TintableIcon.gd
+++ main/common/icon/TintableIcon.gd
@@ -2,3 +2,7 @@ extends TextureRect
 
 func set_tint(color_rgba):
 	$tint_layer.material.set_shader_parameter("tint_color", color_rgba)
+
+func set_textures(main: Texture2D, tint: Texture2D):
+	$solid_layer.texture = main
+	$tint_layer.texture = tint
