502a59b2736ef86eb7ad199ca9581a191a3c7c0f
Aivech <16143392+Aivech@users.noreply.github.com>
2023-12-29 14:45:27 -0500

    Brew UI selector works now

--- main/brewing-scene/brew_soup.gd
+++ main/brewing-scene/brew_soup.gd
@@ -6,19 +6,44 @@ const tintable_icon = preload("res://main/common/icon/TintableIcon.tscn")
 const potion_main = preload("res://resources/textures/ingredients/Bottle.png")
 const potion_tint = preload("res://resources/textures/ingredients/bottle-tint.png")
 
+const text_path = "res://resources/textures/ingredients/"
+const ext = ".png"
+
+
 func start_brew():
+	
+	
 	var potion = tintable_icon.instantiate()
 	potion.set_textures(potion_main, potion_tint)
 	$BrewPanel/ItemScroller/Items.add_child(potion)
-	#$BrewBG.show()
-	pass
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
 	
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	# animal part textures
+	for n in range(1,6):
+		if n == 4: # no texture 4
+			continue
+		var item = texrec_from_resource("animal" + str(n))
+		$BrewPanel/ItemScroller/Items.add_child(item)
+		
+	# fungus texture
+	$BrewPanel/ItemScroller/Items.add_child(texrec_from_resource("fungus1"))
+	
+	# misc ingredient textures
+	for n in range(1,6):
+		$BrewPanel/ItemScroller/Items.add_child(texrec_from_resource("misc"+str(n)))
+	
+	# plant ingredient textures
+	for n in range(1,5):
+		$BrewPanel/ItemScroller/Items.add_child(texrec_from_resource("plant"+str(n)))
+	
+	for n in range(1,6):
+		$BrewPanel/ItemScroller/Items.add_child(texrec_from_resource("misc"+str(n)))
+	for n in range(1,6):
+		$BrewPanel/ItemScroller/Items.add_child(texrec_from_resource("misc"+str(n)))
+	
+func texrec_from_resource(resource:String):
+	var item = TextureRect.new()
+	item.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+	item.custom_minimum_size = Vector2(32,32)
+	var texture_path = text_path + resource + ext
+	item.texture = load(texture_path)
+	return item
