9cfa87ac46c9e91de872d9006304792912002333
Dtanotrew <Dtanotrew12@gmail.com>
2024-01-01 21:00:42 -0500

    Added Attacks w/ Animations and New Controls
    Added 2 basic melee attacks that activate by pressing the F key. Also added support for controller movement and attacking

--- Character/AttackState.gd
+++ Character/AttackState.gd
@@ -1,24 +1,24 @@
 extends State
 
-#@export var return_state : State
-#@export var return_anim: String = "move"
-#@export var attack1_name : String = "attack1"
-#@export var attack2_name : String = "attack2"
-#
-#@onready var timer : Timer = $Timer
-#
-#func state_input(event : InputEvent):
-	#if event.is_action_pressed("attack"):
-		#timer.start()
+@export var return_state : State
+@export var return_anim: String = "move"
+@export var attack1_name : String = "attack1"
+@export var attack2_name : String = "attack2"
 
-#func _on_animation_tree_animation_finished(anim_name):
-	#if anim_name == "attack1":
-		#if timer.is_stopped():
-			#next_state = return_state
-			#playback.travel(return_anim)
-		#else:
-			#playback.travel("attack2")
-			#pass
-	#if anim_name == "attack2":
-		#next_state = return_state
-		#playback.travel(return_anim)
+@onready var timer : Timer = $Timer
+
+func state_input(event : InputEvent):
+	if event.is_action_pressed("attack"):
+		timer.start()
+
+func _on_animation_tree_animation_finished(anim_name):
+	if anim_name == "attack1":
+		if timer.is_stopped():
+			next_state = return_state
+			playback.travel(return_anim)
+		else:
+			playback.travel("attack2")
+			
+	if anim_name == "attack2":
+		next_state = return_state
+		playback.travel(return_anim)
--- Character/GroundState.gd
+++ Character/GroundState.gd
@@ -13,14 +13,14 @@ func state_process(delta):
 func state_input(event : InputEvent):
 	if event.is_action_pressed("jump"):
 		jump()
-	#if event.is_action_pressed("attack"):
-		#attack()
+	if event.is_action_pressed("attack"):
+		attack()
 		
 func jump():
 	character.velocity.y = JUMP_VELOCITY
 	next_state = air_state
 	playback.travel("jump_start")
 
-#func attack():
-	#next_state = attack_state
-	#playback.travel(attack_anim)
+func attack():
+	next_state = attack_state
+	playback.travel(attack_anim)
