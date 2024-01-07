056efc9c41b071a653e1c39a11a75297e8d0426c
Dtanotrew <Dtanotrew12@gmail.com>
2024-01-06 19:59:04 -0500

    Finished Salamanders added test tilesets
    Salamanders work. Still missing player damage/health.

--- Character/HitState.gd
+++ Character/HitState.gd
@@ -7,7 +7,9 @@ class_name HitState
 @export var dead_state : State
 @export var knockback_speed : float = 100.0
 @export var return_state : State
-
+@export var return_anim : String
+@export var hit_anim : String
+@export var death_anim : String
 @onready var timer : Timer = $Timer
 
 # Called when the node enters the scene tree for the first time.
@@ -15,19 +17,21 @@ func _ready():
 	damageable.connect("on_hit", on_damageable_hit)
 
 func on_enter():
+	playback.travel(hit_anim)
 	timer.start()
 
 func on_damageable_hit(node : Node, damage_amount : int, knockback_direction : Vector2):
 	if damageable.health > 0:
 		character.velocity = knockback_speed * knockback_direction
+		print("Salamander Knockback Velocity: " + str(character.velocity))
 		emit_signal("interrupt_state", self)
 	else:
 		emit_signal("interrupt_state", dead_state)
-		playback.travel("dead") #Salamander death animation
+		playback.travel(death_anim) #Replace with animation variables
 
 func on_exit():
 	character.velocity = Vector2.ZERO
 	
-
 func _on_timer_timeout():
 	next_state = return_state
+	playback.travel(return_anim)
